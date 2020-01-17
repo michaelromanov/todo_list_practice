194d16df763e17f8000705de6750aab79e1a3c34
michaelromanov <michael.alexander.romanov@gmail.com>
2020-01-17 00:15:13 -0500

    update

--- logic.js
+++ logic.js
@@ -1,22 +1,22 @@
-//console.log("razzmataz");
-//
-var todos = ["Buy New Turtle"];
+// //console.log("razzmataz");
+// //
+// var todos = ["Buy New Turtle"];
 
-var input = prompt("What would you like to do? (new / list / quit)");
+// var input = prompt("What would you like to do? (new / list / quit)");
 
-while(input !== "quit"){
-  //handle input
-  if(input === "list") {
-    console.log(todos);
-  } else if(input === "new"){
-    //ask for new todo
-    var newTodo = prompt("Enter New Todo");
-    //add to todos array
-    todos.push(newTodo);
-  }
-  //ask for new input
-  input = prompt("What would you like to do? (new / list / quit)")
-}
+// while(input !== "quit"){
+//   //handle input
+//   if(input === "list") {
+//     console.log(todos);
+//   } else if(input === "new"){
+//     //ask for new todo
+//     var newTodo = prompt("Enter New Todo");
+//     //add to todos array
+//     todos.push(newTodo);
+//   }
+//   //ask for new input
+//   input = prompt("What would you like to do? (new / list / quit)")
+// }
 
-console.log("Ok, you quit the app!  Come back soon!");
+// console.log("Ok, you quit the app!  Come back soon!");
 
