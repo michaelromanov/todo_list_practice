74dc1ffaad85a237e770042ac5728bcd15d9b679
michaelromanov <michael.alexander.romanov@gmail.com>
2020-01-19 02:44:09 -0500

    update

--- logic.js
+++ logic.js
@@ -20,5 +20,61 @@
 
 // console.log("Ok, you quit the app!  Come back soon!");
 
+// var movies = [
+//   {title: "In Bruges", 
+//    hasWatched: true, 
+//    rating: 5
+//   }, 
+//   {title: "Frozen", 
+//    hasWatched: false, 
+//    rating: 4.5
+//   },
+//   {title: "Les Mis", 
+//    hasWatched: false, 
+//    rating: 3.5
+//   }
+// ]
 
-//
+// movies.forEach(function(movie){
+//   var result = "You have ";
+//   if(movie.hasWatched){
+//     result += "watched "
+//   } else {
+//     result += "not seen "
+//   }
+//     result += "\"" + movie.title + "\" -"
+//     result += movie.rating + " stars";
+//   console.log(result);
+// })
+
+
+var movies = [
+  {title: "In Bruges", 
+   hasWatched: true, 
+   rating: 5
+  }, 
+  {title: "Frozen", 
+   hasWatched: false, 
+   rating: 4.5
+  },
+  {title: "Les Mis", 
+   hasWatched: false, 
+   rating: 3.5
+  }
+]
+
+function buildString(movie){
+  var result = "You have ";
+  if(movie.hasWatched){
+    result += "watched "
+  } else {
+    result += "not seen "
+  }
+    result += "\"" + movie.title + "\" -"
+    result += movie.rating + " stars";
+    return result;
+}
+
+movies.forEach(function(movie){
+  console.log(buildString(movie));
+})
