79c4d987dae4df3f21127cbfe580af82420e4b55
michaelromanov <michael.alexander.romanov@gmail.com>
2020-01-25 01:16:14 -0500

    update

--- 15_DOM_ selector.js
+++ 15_DOM_ selector.js
@@ -16,5 +16,75 @@ var li = document.querySelector("li a.special");
 var tags = document.querySelectorAll('h1');
 var tags = documnet.querySelectorAll(".bolded");
 
+// DOM Selector Style - Style property one way to manipulate an element's style
+//Select:
+var tag = document.getElementById("highlight");
+//Manipulate:
+tag.style.color = "blue";
+tag.style.border = "10px solid red";
+tag.style.fontSize = "70px";
+tag.style.background = "yellow";
+tag.style.marginTop = "200px";
+// The above code is repetitive and not DRY aka Do Not Repeat Yourself
+//Programming is based on separation of concerns and while Structure, Behavior and Presentation
+//can overlap they are indeed separate concerns.  
+//So rather than selecting a tag and changing it in JS, we create a class in CSS and add the 
+//class to the selecte element.
+var tag = document.getElementsById("highlight");
+tag.style.color = "blue";
+tag.style.border = "10px solid red";
+
+//do the below instead
+//define a class in CSS:
+// .some-class {
+//   color: blue;
+//   border: 10px, solid, red;
+// }
+
+var tag = document.getElementById("highlight");
+//Add the new class to the selected Element
+tag.classList.add("some-class");
+//classList is a read only list that contains the classes for a given element.  It is not an array.
+//classList:
+// .another-class {
+//   color: purple;
+//   fontSize: 76px;
+// }
+
+var tag = document.querySelector("h1");
+//Add a class to the selected element
+tag.classList.add("another-class");
+//Remove a class 
+tag.classList.remove("another-class");
+//Toggle a class
+tag.classList.toggle("another-class");
+
+// So the JS turns on or off a specific part of the CSS rather than manually manipulating individual properties.
+
+//textContent - returns a string of all the text contained in a given element
+// <p>This is an <strong>awesome</strong> paragraph</p>
+//Select the <p> tag: 
+var tag = document.querySelector("p");
+//Retrieve the textContent:
+tag.textContent //"This is an awesome paragraph"
+//alter the textContent: 
+tag.textContent = "blah blah blah";
+
+//innerHTML - similar to textContent, except it returns a string of all the HTML contained in a given element
+// <p>This is an <strong>awesome</strong> paragraph</p>
+//Select the <p> tag: 
+var tag = document.querySelector("p");
+tag.innerHTML
+//This is an <strong>awesome</strong> paragraph
+var ul = document.querySelector("ul");
+document.querySelector("h1").textContent = "End of this lesson";
+documnet.body.textContent = "<h1>Goodbye</h1>";
+documnet.body.innerHTML = "<h1>Goodbye</h1>";
+
+
+
+
+
+
 
  
