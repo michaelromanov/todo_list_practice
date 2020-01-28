93a3d44f0b28eb84463b2b99582254f9df6a988b
michaelromanov <michael.alexander.romanov@gmail.com>
2020-01-28 02:42:30 -0500

    update

--- 16_DOM_ADVANCED_DOM_MANIPULATION.js
+++ 16_DOM_ADVANCED_DOM_MANIPULATION.js
@@ -1,5 +1,6 @@
 // alert("CONNECTED!!!!!")
 
+//element.addEventListener(type, functionToCall);
 // var button = document.querySelector("button");
 // button.addEventListener("click", function(){
 //   console.log("Someone clicked the button!");
@@ -13,3 +14,15 @@ var paragraph = document.querySelector("p");
 button.addEventListener("click", function(){
   paragraph.textContent = "Someone clicked the button!";
 });
+
+var h1 = document.querySelector("h1");
+h1.addEventListener("click", function(){
+  //alert("h1 was clicked");
+  h1.style.background = "orange";
+});
+
+var ul = document.querySelector("ul");
+ul.addEventListener("click", function(){
+  ul.style.background = "yellow";
+});
+
