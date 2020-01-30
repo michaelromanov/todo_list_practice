20e7267bbf439dbf9725a34e5c3bfbbb33a890d8
michaelromanov <michael.alexander.romanov@gmail.com>
2020-01-30 01:27:01 -0500

    update

--- 16_Color_Toggle_Exercise.js
+++ 16_Color_Toggle_Exercise.js
@@ -1,3 +1,31 @@
 // alert("Connected!")
 
 var button = document.querySelector("button");
+var body = document.querySelector("body");
+var isPurple = false;
+
+
+// button.addEventListener("click", function(){
+//   //alert("clicked");
+
+//   //if white
+//     //make it purple
+//   // else make it white
+
+//   if(isPurple){
+//     document.body.style.background = "white";
+//     isPurple = false;
+//   } else {
+//     document.body.style.background = "purple";
+//     isPurple = true;
+//   }  
+
+//   //isPurple = !isPurple;
+
+//   //document.body.style.background = "purple";
+   
+// });
+
+button.addEventListener("click", function(){
+  document.body.classList.toggle("purple"); 
+})
